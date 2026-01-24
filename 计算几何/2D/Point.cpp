6e1726a5571ccee7285d50df9334d8dd5acdfaaf
fapig1 <3540874737@qq.com>
2026-01-24 11:37:58 +0800

    把计算几何传参的int n优化了

--- 计算几何/2D/Point.cpp
+++ 计算几何/2D/Point.cpp
@@ -40,7 +40,8 @@ double ddis(Pt a){
 }
 
 double Angle(Pt a, Pt b){
-    return acos(a * b / dis(a) / dis(b));
+    double val = a * b / dis(a) / dis(b);
+    return acos(max(-1.0, min(1.0, val))); 
 }
 
 //在直线的哪边
@@ -132,7 +133,8 @@ double Triangle_area(Pt A, Pt B, Pt C){
 // 因为叉积求得的三角形面积是有向的, 在外面的面积可以正负抵消掉
 // 所以能够求任意多边形面积(凸, !凸)
 // p[]下标从 0 开始, 长度为 n
-double area(vector<Pt>& p, int n){
+double area(vector<Pt>& p){
+    int n = p.size();
     double S = 0;
     for(int i = 1; i <= n - 2; i++){
         S += (p[i] - p[0]) % (p[i + 1] - p[0]);
@@ -148,7 +150,8 @@ double area(vector<Pt>& p, int n){
 // 点在多边形外, 返回 0
 
 // p[] 的下标从 0 开始, 长度为 n
-int InPolygon(Pt P, vector<Pt>& p, int n) {
+int InPolygon(Pt P, vector<Pt>& p) {
+    int n = p.size();
     bool flag = false;		
     for (int i = 0, j = n - 1; i < n; j = i++) {
         Pt p1 = p[i], p2 = p[j];
@@ -160,7 +163,8 @@ int InPolygon(Pt P, vector<Pt>& p, int n) {
     return flag;
 }
 //是否为凸多边形
-bool Is_contex(vector<Pt>& p, int n){
+bool Is_contex(vector<Pt>& p){
+    int n = p.size();
     bool s[3] = {0, 0, 0};
     for (int i = 0, j = n - 1, k = n - 2; i < n; k = j, j = i++) {
         int cnt = sgn((p[i] - p[j]) % (p[k] - p[j])) + 1;
@@ -206,15 +210,21 @@ int LWC(Pt A, Pt B, Circle c) {
 }
 
 //直线和圆的交点
-pair<Pt, Pt> Intersection_line_circle(Pt A, Pt B, Circle c) {
+vector<Pt> Intersection_line_circle(Pt A, Pt B, Circle c) {
     Pt AB = B - A;
-    Pt pr = A + AB * ((c.o - A) * AB / (AB * AB));
-    double base = sqrt(c.r * c.r - ((pr - c.o) * (pr - c.o)));
+    double len2 = AB * AB;
+    Pt pr = A + AB * ((c.o - A) * AB / len2);
+    double d2 = ddis(pr - c.o);
     
-    if (sgn(base) == 0) return make_pair(pr, pr);
+    int status = sgn(d2 - c.r * c.r);
+    if (status > 0) return {}; // 相离，返回空 vector
 
-    Pt e = AB / sqrt(AB * AB);
-    return make_pair(pr + e * base, pr - e * base);
+    double base = sqrt(max(0.0, c.r * c.r - d2));
+    if (status == 0) return {pr}; // 相切，返回 1 个点
+
+    // 相交，返回 2 个点
+    Pt e = AB / sqrt(len2);
+    return {pr + e * base, pr - e * base};
 }
 //圆与圆的位置关系
 // 相离, 返回 -1
@@ -242,29 +252,50 @@ int Circle_with_circle(Circle A, Circle B) {
 // 相切, 返回两个一样的相切点
 
 // 要先判断是否相交或相切再调用
-pair<Pt, Pt> Intersection_circle_circle(Circle A, Circle B) {
-    Pt AB = B.o - A.o;
-    double d = dis(AB);
-    double a = acos((A.r * A.r + d * d - B.r * B.r) / (2.0 * A.r * d));
-    double t = atan2(AB.y, AB.x);
-    Pt x(A.r * cos(t + a), A.r * sin(t + a));
-    Pt y(A.r * cos(t - a), A.r * sin(t - a));
-    return make_pair(A.o + x, A.o + y);
+vector<Pt> Intersection_circle_circle(Circle A, Circle B) {
+    double d = dis(A.o - B.o);
+    // 情况 1: 相离或内含 (无交点)
+    if (sgn(d - (A.r + B.r)) > 0 || sgn(d - fabs(A.r - B.r)) < 0) {
+        return {};
+    }
+
+    // 情况 2: 相切 (1 个交点)
+    if (sgn(d - (A.r + B.r)) == 0 || sgn(d - fabs(A.r - B.r)) == 0) {
+        Pt e = (B.o - A.o) / d;
+        return {A.o + e * A.r};
+    }
+
+    // 情况 3: 相交 (2 个交点)
+    double a = acos(max(-1.0, min(1.0, (A.r * A.r + d * d - B.r * B.r) / (2.0 * A.r * d))));
+    double t = atan2(B.o.y - A.o.y, B.o.x - A.o.x);
+    return {
+        A.o + Pt(A.r * cos(t + a), A.r * sin(t + a)),
+        A.o + Pt(A.r * cos(t - a), A.r * sin(t - a))
+    };
 }
 //求圆外一点对圆的两个切点
-pair<Pt, Pt> TangentPt_Pt_circle(Pt p, Circle c) {
-    double d = dis(p - c.o);
-    double l = sqrt(d * d - c.r * c.r);
-    Pt e = (c.o - p) / d;
-    double angle = asin(c.r / d);
+vector<Pt> TangentPt_Pt_circle(Pt p, Circle c) {
+    double d2 = ddis(p - c.o);
+    int status = sgn(d2 - c.r * c.r);
+    
+    if (status < 0) return {}; // 点在圆内，无切点
+    if (status == 0) return {p}; // 点在圆上，切点就是自身
 
-    Pt t1(sin(angle), cos(angle));
-    Pt t2(sin(-angle), cos(-angle));
-    Pt e1(e % t1, e * t1);
-    Pt e2(e % t2, e * t2);
-    e1 = e1 * l + p;
-    e2 = e2 * l + p;
-    return make_pair(e1, e2);
+    // 点在圆外，2 个切点
+    double d = sqrt(d2);
+    double l = sqrt(d2 - c.r * c.r);
+    double angle = asin(max(-1.0, min(1.0, c.r / d)));
+    Pt e = (c.o - p) / d;
+
+    // 旋转向量得到切点方向
+    auto rotate = [](Pt v, double ang) {
+        return Pt(v.x * cos(ang) - v.y * sin(ang), v.x * sin(ang) + v.y * cos(ang));
+    };
+
+    return {
+        p + rotate(e, angle) * l,
+        p + rotate(e, -angle) * l
+    };
 }
 
 //求三角形外接圆
@@ -288,6 +319,7 @@ Circle get_incircle(Pt A, Pt B, Pt C) {
 }
 // 要保证传入的点是整点
 //线段上的整点个数
+//注意按照要求修改返回值
 int IntegerPt_on_seg(Pt A, Pt B) {
     int x = abs(A.x - B.x);
     int y = abs(A.y - B.y);
@@ -299,7 +331,8 @@ int IntegerPt_on_seg(Pt A, Pt B) {
 // 点需要是顺时针(逆时针)给出
 
 // p[] 下标从 0 开始, 长度为 n
-int IntegerPt_on_polygon(vector<Pt>& p, int n) {
+int IntegerPt_on_polygon(vector<Pt>& p) {
+    int n = p.size();
     int res = 0;
     for (int i = 0, j = n - 1; i < n; j = i++) {
         int x = abs(p[i].x - p[j].x);
@@ -309,30 +342,47 @@ int IntegerPt_on_polygon(vector<Pt>& p, int n) {
     return res;
 }
 // 返回不包括边界的, 多边形**内**整点个数
-int IntegerPt_in_polygon(vector<Pt>& p, int n) {
-    double A = area(p, n);
-    double B = IntegerPt_on_polygon(p, n);
+int IntegerPt_in_polygon(vector<Pt>& p) {
+    int n = p.size();
+    double A = area(p);
+    double B = IntegerPt_on_polygon(p);
     return A - B / 2 + 1;
 }
-//级角排序
-void polarSort(vector<Pt>& Pts, const Pt& center){
+
+int getQuadrant(Pt p, Pt center) {
+    double x = p.x - center.x;
+    double y = p.y - center.y;
+    if (x > 0 && y >= 0) return 1; // 第一象限
+    if (x <= 0 && y > 0) return 2; // 第二象限
+    if (x < 0 && y <= 0) return 3; // 第三象限
+    if (x >= 0 && y < 0) return 4; // 第四象限
+    return 0;
+}
+
+void polarSort(vector<Pt>& Pts, const Pt& center) {
     sort(Pts.begin(), Pts.end(), [&center](const Pt& a, const Pt& b) {
-        Pt va = a - center;
-        Pt vb = b - center;
-        double cross = va % vb;
+        int quadA = getQuadrant(a, center);
+        int quadB = getQuadrant(b, center);
         
-        if (fabs(cross) < eps) {
-            double distA = va * va;
-            double distB = vb * vb;
-            return distA < distB;
+        if (quadA != quadB) {
+            return quadA < quadB;
         }
-        return cross > 0; 
+        
+        // 同一象限内使用叉积
+        double cp = (a - center) % (b - center);
+        if (fabs(cp) > eps) {
+            return cp > 0; // 逆时针排序
+        }
+        
+        // 共线时，距离近的在前
+        return ddis(a - center) < ddis(b - center);
     });
 }
 
 //凸包算法
-vector<Pt> Andrew(vector<Pt>& p, int n){
+vector<Pt> Andrew(vector<Pt>& p){
     sort(p.begin(), p.end());
+    int n = p.size();
     vector<Pt> ans;
     for(int i = 0; i < n; i++){
         while(ans.size() >= 2 && Cross(ans[ans.size() - 2], ans.back(), p[i]) <= 0){
@@ -352,45 +402,4 @@ vector<Pt> Andrew(vector<Pt>& p, int n){
     if(ans.size() > 1) ans.pop_back();
     return ans;
 
-}
-
-
-
-
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0), cout.tie(0);
-
-    int n, q;
-    cin >> n;
-    vector<Pt> p(n);
-    for(int i = 0; i < n; i++){
-        cin >> p[i].x >> p[i].y;
-    }
-
-    cin >> q;
-    while(q--){
-        Line l;
-        cin >> l.s.x >> l.s.y >> l.e.x >> l.e.y;
-        vector<Pt> P;
-        for(int i = 0; i < n; i++){
-            if(Cross(p[i], l) > 0) P.push_back(p[i]);
-        }
-
-        for(int i = 0; i < n; i++){
-            if(Intersect_line_seg(p[i], p[(i + 1) % n], l.s, l.e)){
-                P.push_back(Inter_Line_Pt(p[i], p[(i + 1) % n], l.s, l.e));
-            }
-        }
-
-
-        if(P.size() < 3){
-            cout << fixed << setprecision(15) << 0.0 << '\n';
-            continue;
-        }
-        double ans = area(P, P.size());
-        cout << fixed << setprecision(15) << ans << '\n';
-    }
-    
-    return 0;
 }
